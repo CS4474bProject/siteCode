4db80f2899290b71504aeebbfe2bcef695496d6f
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-15 23:31:44 -0400

    Added error checking to server side

--- StudentCenter2.0/public_html/php/sqlite.php
+++ StudentCenter2.0/public_html/php/sqlite.php
@@ -1,7 +1,12 @@
 <?php
     //First, we get the data.
     $query = $_POST['sql'];
-
+    
+    //Ensures we don't have error.
+    if ($query == ""){
+        die("error");
+    }
+    
     //Next we run sqlite.
     $dir = 'sqlite:../database/data.db';
     $db = new PDO($dir) or die("error");
