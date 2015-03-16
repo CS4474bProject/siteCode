e7e238647610679eec35645ab6290763eda19d49
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-15 23:16:25 -0400

    Testing PHP function

--- StudentCenter2.0/public_html/php/sqlite.php
+++ StudentCenter2.0/public_html/php/sqlite.php
@@ -1,3 +1,12 @@
 <?php
-    echo "apple";
+    //First, we get the data.
+    //$query = $_POST['sql'];
+    
+    $query = 'SELECT * FROM Users;';
+    
+    //Next we run sqlite.
+    $db = new SQLite3('../database/data.db');
+    $results = $db->query($query);
+    
+    echo $results;
 ?>
