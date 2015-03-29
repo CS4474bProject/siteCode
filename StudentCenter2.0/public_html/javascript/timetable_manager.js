1f4da982d811d2af552290e2efdf1af89d2bb42c
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-29 01:44:24 -0400

    Improved course system

--- StudentCenter2.0/public_html/javascript/timetable_manager.js
+++ StudentCenter2.0/public_html/javascript/timetable_manager.js
@@ -8,23 +8,42 @@ function getCourses(){
 }
 
 function receiveCourses(data){
-    alert(data);
-    alert(data[0]['CourseNum']);
-    
     //Generate the buttons.
     lastNum = 0;
     for (i = 0; i < data.length; i++){
         //Gets the last array element.
         element = data[i];
-       
+        
+        //Manages time elements.
+        end = 0;
+        start = 0;
+        if (element['EndTime'].split(':')[0] > 12)
+            end = element['EndTime'].split(':')[0] + 12;
+        else
+            end = element['EndTime'].split(':')[0];
+        if (element['StartTime'].split(':')[0] > 12)
+            start = element['StartTime'].split(':')[0] + 12;
+        else
+            start = element['StartTime'].split(':')[0];
+        startTime = new Date(2000, 0, 1, start, 
+            element['StartTime'].split(':')[1].substring(0,1));
+        endTime = new Date(2000, 0, 1, end, 
+            element['EndTime'].split(':')[1].substring(0,1));
+        
+        //Gets the difference between the time.
+        diff = endTime - startTime;
+        duration = Math.floor(diff / 1000 / 60 / 60) + 
+                   (Math.floor(msec / 1000 / 60) / 60);
+        alert(duration);
+        
         //Checks if the last course was also a time.
         if (lastNum === element['CourseNum']){
             $( '#' + lastNum ).attr('onClick', $( '#' + lastNum ).attr('onClick') +
-                    'addCourse("' + element['CourseName'] + '","' + element['Subject'] + '", "' + element['CourseCode'] + '", "SSC-2028", ' + element['Semester'] + ', "230p", "Mon", 2);');
+                    'addCourse("' + element['CourseName'] + '","' + element['Subject'] + '", "' + element['CourseCode'] + '", "SSC-2028", ' + element['Semester'] + ', "230p", ' + element['DayOfWeek'] + ', 2);');
         } else {
             $( '#buttons' ).html($( '#buttons' ).html() + 
                     '<button type="button" id="' + element['CourseNum'] +
-                    '" onclick=\'addCourse("' + element['CourseName'] + '","' + element['Subject'] + '", "' + element['CourseCode'] + '", "SSC-2028", ' + element['Semester'] + ', "230p", "Mon", 2);\'>' +
+                    '" onclick=\'addCourse("' + element['CourseName'] + '","' + element['Subject'] + '", "' + element['CourseCode'] + '", "SSC-2028", ' + element['Semester'] + ', "230p", ' + element['DayOfWeek'] + ', 2);\'>' +
                     element['CourseName'] + '</button>');
             lastNum = element['CourseNum'];
         }
