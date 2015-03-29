e1165a7ad9176ba9f93465c8fa5307269e904117
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-28 21:15:08 -0400

    Added in code to add courses to mock timetable.

--- StudentCenter2.0/public_html/javascript/timetable_manager.js
+++ StudentCenter2.0/public_html/javascript/timetable_manager.js
@@ -2,4 +2,35 @@ $(function(){
       $("#timetable").load("timetable.html"); 
 });
 
+function addCourse(name, code, semester, startTime, endTime, day, len){
+    //First, we get the first time element.
+    elementCode = '#';
+    if (semester === 2) elementCode += 2;
+    elementCode += day + startTime;
+    
+    //Selects the first element.
+    startCell = $(elementCode);
+    
+    //We delete the rows below.
+    index = startCell.index();  
+    for (i = 1; i < (len / 0.5); i++) { 
+        //Gets the next cell.
+        nextCell = $(elementCode).closest('tr').next().children().eq(index);
+        
+        //Removes it.
+        if (i !== 1)
+            $(elementCode).remove();
+        
+        //Gets important next variables.
+        index = nextCell.index();
+        elementCode = '#' + nextCell.attr('id');
+    }
 
+    //Removes final element.
+    $(elementCode).remove();
+    
+    //Merges the new cells together.
+    startCell.attr('rowspan', len / 0.5);
+    
+    //Add in information to table.
+}
