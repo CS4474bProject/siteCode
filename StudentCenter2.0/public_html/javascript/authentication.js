5133ba7fd680c0fe9b381bd31b12782a3e9569c1
Bryan Muscedere <bmuscede@uwo.ca>
2015-03-30 00:17:15 -0400

    Changed course enrollment

--- StudentCenter2.0/public_html/javascript/authentication.js
+++ StudentCenter2.0/public_html/javascript/authentication.js
@@ -23,6 +23,7 @@ function authError(data){
 
 function authSuccess(data){
     //We create a login cookie.
+    alert(data[0]['UserName']);
     document.cookie = 'username=' + data[0]['UserName'] + '; path=/';
     document.cookie = 'name=' + data[0]['FirstName'] + '; path=/';
     
