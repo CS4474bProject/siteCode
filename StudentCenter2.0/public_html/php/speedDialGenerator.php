1ab4f7eb755e839e0930a3d20866da43cbdf4826
Spencer Hanley <shanley3@uwo.ca>
2015-03-31 11:41:24 -0400

    Bug Fixes to PHP, New Accordion
    Bug fixes for the new PHP file, and accordion for the quick schedule
    viewer

--- StudentCenter2.0/public_html/php/speedDialGenerator.php
+++ StudentCenter2.0/public_html/php/speedDialGenerator.php
@@ -1,7 +1,7 @@
 <?php
 
-$htmlFileInstance = fopen(“speedDial.html”, “w”) or die(“Unable to open file!”);
-$codeInjection = “HTML Updated content will go here…”;
+$htmlFileInstance = fopen("speedDial.html", "w") or die("Unable to open file!");
+$codeInjection = "HTML Updated content will go here…";
 fwrite($htmlFileInstance, $codeInjection);
 fclose($htmlFileInstance);
 
