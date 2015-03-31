2196ef8a59275629f085bf5e5db08505d35078a9
Spencer Hanley <shanley3@uwo.ca>
2015-03-31 10:56:15 -0400

    Added new PHP file
    Added a new PHP file to generate an HTML file dynamically for the Speed
    Dial functionality - Not fully implemented, just makes a new file

---
+++ StudentCenter2.0/public_html/php/speedDialGenerator.php
@@ -0,0 +1,8 @@
+<?php
+
+$htmlFileInstance = fopen(“speedDial.html”, “w”) or die(“Unable to open file!”);
+$codeInjection = “HTML Updated content will go here…”;
+fwrite($htmlFileInstance, $codeInjection);
+fclose($htmlFileInstance);
+
+?>
