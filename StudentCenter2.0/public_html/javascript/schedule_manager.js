89f5d4d58776c7fbfe15b0400b082a4c941da6d2
Bryan Muscedere <bmuscede@uwo.ca>
2015-04-04 14:46:40 -0400

    Updated jspdf libraries

--- StudentCenter2.0/public_html/javascript/schedule_manager.js
+++ StudentCenter2.0/public_html/javascript/schedule_manager.js
@@ -6,5 +6,21 @@ function generatePDF(){
     //Creates the new PDF object.
     var pdf = new jsPDF();
  
-    
+         
+    var elementHandler = {
+        '#ignorePDF': function (element, renderer) {
+            return true;
+        }
+    };
+
+    var source = window.document.getElementById("timetable");
+    pdf.fromHTML(
+        source,
+        15,
+        15,
+        {
+            'width': 180,'elementHandlers': elementHandler
+        });
+
+    pdf.output("dataurlnewwindow");
 }
