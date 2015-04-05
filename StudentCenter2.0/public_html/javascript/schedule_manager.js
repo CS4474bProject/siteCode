4d38305cdb9287efa1c4a0b62a12bafa787b87e7
Bryan Muscedere <bmuscede@uwo.ca>
2015-04-04 22:16:21 -0400

    Working on pdf generation

--- StudentCenter2.0/public_html/javascript/schedule_manager.js
+++ StudentCenter2.0/public_html/javascript/schedule_manager.js
@@ -8,14 +8,14 @@ function generatePDF(){
          
     // We'll make our own renderer to skip this editor
     var specialElementHandlers = {
-            '#editor': function(element, renderer){
+            '#title': function(element, renderer){
                     return true;
             }
     };
 
     // All units are in the set measurement for the document
     // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
-    pdf.fromHTML($('body').get(0), 15, 15, {
+    pdf.fromHTML($('#timetable').get(0), 15, 15, {
             'width': 170, 
             'elementHandlers': specialElementHandlers
     });
